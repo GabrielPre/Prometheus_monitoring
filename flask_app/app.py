d755bd35ec85c9978a3a2f5b377c6c69c5bab3ee
Gabriel <Gabriel.precigout@efrei.net>
2022-02-19 19:47:44 +0100

    Updating app.py to match requirements

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,34 +1,45 @@
 import time
+import random
 import redis
-import requests
 from flask import Flask, request
-from prometheus_client import start_http_server
+from prometheus_client import start_http_server, Counter, Gauge, Summary, Histogram
 
-app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 start_http_server(8010)
+cache=redis.Redis(host='redis', port=6379)
+app= Flask(__name__)
+
+REQUESTS = Counter('hello_worlds_total', 'Hello Worlds requested.')
+EXCEPTIONS = Counter('hello_world_exceptions_total', 'Exceptions serving Hello World.')
+INPROGRESS = Gauge('hello_worlds_inprogress', 'Number of Hello Worlds in progress.')
+LAST = Gauge('hello_world_last_time_seconds', 'The last time a Hello World was served.')
+LATENCY = Summary('hello_world_latency_seconds', 'Time for a request Hello World.')
 
 def get_hit_count():
-    retries = 5
+    retries= 5
     while True:
         try:
             return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
+        except redis.exceptions.ConnectionError as exc :
+            if retries == 0 :
                 raise exc
-            retries -= 1
+            retries -=1
             time.sleep(0.5)
+            
+@app.route('/metrics')
+def hello():
+    start = time.time()
+    REQUESTS.inc()
+    INPROGRESS.inc()
+    LAST.set(start)
 
-def get_mean(argument):
-    if (argument == None):
-        return "no list was given"
-    argument_split = argument.split(",")
-    argument_split = list(map(int, argument_split))
-    mean = sum(argument_split)/len(argument_split)
-    return mean
+    time.sleep(random.random())
+    with EXCEPTIONS.count_exceptions(): 
+        # Une chance sur deux
+        if random.choice((True, False)):
+            INPROGRESS.dec()
+            LATENCY.observe(time.time() - start)
+            return "hello to my sample flask app, I have been seen {} times \n".format(get_hit_count())
 
-
-@app.route('/')
-def home():
-    argument = request.args.get("argument")
-    return "hello to my sample flask app, I have been seen {} times\n If you want to calculate the mean add your numbers in the url like this : http://localhost:5000/?argument=1,2,3,4\nThe mean of your numbers in arguments is {}.\n".format(get_hit_count(), get_mean(argument))
+        INPROGRESS.dec()
+        LATENCY.observe(time.time() - start)
+        raise Exception()
